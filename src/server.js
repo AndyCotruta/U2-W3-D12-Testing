a01a1597877ed8d63cba3fd0c1ce2d6b7aeaf7fe
AndyCotruta <acotruta18@gmail.com>
2023-02-14 16:59:46 +0100

    initial setup

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+import productsRouter from "./api/products/index.js";
+import { badRequestHandler, genericErrorHandler } from "./errorHandlers.js";
+
+const server = express();
+
+// ************************************* MIDDLEWARES *******************************
+server.use(cors());
+server.use(express.json());
+
+// *************************************** ENDPOINTS *******************************
+server.use("/products", productsRouter);
+
+// ************************************ ERROR HANDLERS *****************************
+server.use(badRequestHandler);
+server.use(genericErrorHandler);
+
+export default server;
